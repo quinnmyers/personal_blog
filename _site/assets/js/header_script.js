fba6e7fd4aca6b33c3a77f04f5053d52bbb8ab5f
Quinn <quinnsmyers@gmail.com>
2016-08-18 00:01:32 -0700

    added all pages, waiting for content

---
+++ _site/assets/js/header_script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".hamburger").on('click', function() {
+    $(".mobile-nav").toggleClass("is-active");
+  });
+});
+
+
+//$(document).ready(function () {
+//  $(".item").on('click', function() {
+  //  $(".mobile-nav").removeClass("is-open");
+//  });
+//});
